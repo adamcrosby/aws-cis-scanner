59c6aee4efced318b8148ca22205c21222b68045
Adam Crosby <adam@uptill3.com>
2016-09-04 10:55:33 -0400

    initial multi-region support capability

--- aws-cis-scanner.go
+++ aws-cis-scanner.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/adamcrosby/aws-cis-scanner/benchmark"
 	"github.com/adamcrosby/aws-cis-scanner/utility/accounts"
+	"github.com/adamcrosby/aws-cis-scanner/utility/regions"
 	"github.com/adamcrosby/aws-cis-scanner/utility/report"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -23,49 +24,45 @@ import (
 )
 
 func main() {
-	//todo: determine which regions to run, for now just do commercial us-east
-	//iam.New(session.New(), &aws.Config{Region: aws.String("us-east-1")}) call style
 	var regionPtr string
 	const (
-		defaultRegion   = "us-east-1"
+		defaultRegion   = regions.USEast1
 		regionFlagUsage = "AWS Region in standard shorthand format (eg: 'us-east-1' or 'us-west-2').  Default is \"us-east-1\"."
 	)
 	flag.StringVar(&regionPtr, "region", defaultRegion, regionFlagUsage)
 	flag.StringVar(&regionPtr, "r", defaultRegion, regionFlagUsage+" (shorthand)")
 	flag.Parse()
 
-	sess, err := session.NewSession()
-	if regionPtr == defaultRegion {
-		fmt.Println("No region specified, defaulting to region: ", regionPtr)
+	// if region is govcloud or china, special handling:
+	if regionPtr == regions.CNNorth1 {
+		fmt.Println("Isolated Regions not yet supported.")
+		os.Exit(1)
 	}
 
+	// figure out the list of regions to iterate over
+	var regionsList []string
+	if regionPtr == regions.GovCloud {
+		regionsList = regions.GovRegions
+
+	} else {
+
+		regionsList = regions.CommercialRegions
+
+	}
+
+	// Create a new session
+	sess, err := session.NewSession()
 	if err != nil {
 		panic(err)
 	}
+
 	status := benchmark.Status{}
-
-	IAM := iam.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-
-	awsAccounts := accounts.GetAccounts(IAM)
-	awsPasswordPolicy := accounts.GetPasswordPolicy(IAM)
-	status.Finding1_15 = accounts.UserPoliciesExist(awsAccounts, IAM)
-	status = benchmark.DoIAMChecks(status, awsAccounts, awsPasswordPolicy)
-
-	// Setup for the Logging section of checks (2.1 - 2.8)
-	CT := cloudtrail.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-	s3Svc := s3.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-	cf := configservice.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-	kmsSvc := kms.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-	cwlogs := cloudwatchlogs.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-	cw := cloudwatch.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-	snsSvc := sns.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-	ec2Svc := ec2.New(sess, &aws.Config{Region: aws.String(regionPtr)})
-
-	status = benchmark.LoggingChecks(kmsSvc, cf, s3Svc, CT, status)
-	status = benchmark.MonitoringChecks(snsSvc, cw, cwlogs, CT, status)
-	status = benchmark.DoNetworkChecks(ec2Svc, status)
-
-	//fmt.Println(status)
+	for i := range regionsList {
+		fmt.Println("Region is: ", regionsList[i])
+		conf := aws.Config{Region: aws.String(regionsList[i])}
+		fmt.Printf("Conf says: %+v \n", conf)
+		status = checkRegion(status, sess, conf)
+	}
 
 	printTemplate(status)
 }
@@ -84,4 +81,29 @@ func printTemplate(status benchmark.Status) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func checkRegion(status benchmark.Status, sess *session.Session, conf aws.Config) benchmark.Status {
+
+	iamSvc := iam.New(sess, &conf)
+	ctSvc := cloudtrail.New(sess, &conf)
+	s3Svc := s3.New(sess, &conf)
+	cfSvc := configservice.New(sess, &conf)
+	kmsSvc := kms.New(sess, &conf)
+	cwlogsSvc := cloudwatchlogs.New(sess, &conf)
+	cwSvc := cloudwatch.New(sess, &conf)
+	snsSvc := sns.New(sess, &conf)
+	ec2Svc := ec2.New(sess, &conf)
+
+	awsAccounts := accounts.GetAccounts(iamSvc)
+	awsPasswordPolicy := accounts.GetPasswordPolicy(iamSvc)
+	status.Finding1_15 = accounts.UserPoliciesExist(awsAccounts, iamSvc)
+	status = benchmark.DoIAMChecks(status, awsAccounts, awsPasswordPolicy)
+
+	// Setup for the Logging section of checks (2.1 - 2.8)
+
+	status = benchmark.LoggingChecks(kmsSvc, cfSvc, s3Svc, ctSvc, status)
+	status = benchmark.MonitoringChecks(snsSvc, cwSvc, cwlogsSvc, ctSvc, status)
+	status = benchmark.DoNetworkChecks(ec2Svc, status)
+	return status
 }
--- utility/regions/regions.go
+++ utility/regions/regions.go
@@ -1,15 +1,39 @@
 package regions
 
-type ServiceParams struct {
-	useCommercialRegions bool
-	commercialEndpoint   string
-	useUSGovCloudRegion  bool
-	usGovCloudEndpoint   string
-	useCNRegion          bool
-	cnRegionEndpoint     string
-	useC2SRegion         bool
-	c2SRegionEndpoint    string
-}
+const (
+	// APNorthEast1 is the Tokyo region
+	APNorthEast1 = "ap-northeast-1"
+	// APNorthEast2 is the Seoul region
+	APNorthEast2 = "ap-northeast-2"
+	//APSouth1     is the  Mumbai region
+	APSouth1 = "ap-south-1"
+	//APSouthEast1 is the  Singapore region
+	APSouthEast1 = "ap-southeast-1"
+	//APSouthEast2 is the  Sydney region
+	APSouthEast2 = "ap-southeast-2"
+	//CNNorth1     is the  Beijing region
+	CNNorth1 = "cn-north-1"
+	//EUCentral1   is the  Frankfurt region
+	EUCentral1 = "eu-central-1"
+	//EUWest1      is the  Ireland region
+	EUWest1 = "eu-west-1"
+	//GovCloud is the US GovCloud (Oregon) region
+	GovCloud = "us-gov-west-1"
+	//SAEast1 is the Sao  Paulo region
+	SAEast1 = "sa-east-1"
+	//USEast1  is the N. Virginia region
+	USEast1 = "us-east-1"
+	//USWest1 is the N. California region
+	USWest1 = "us-west-1"
+	//USWest2 is the Oregon
+	USWest2 = "us-west-2"
+)
 
-const CommercialEndpointDefault = "us-east-1"
-const USGovCloudEndpointDefault = "us-gov-west-1"
+// CommercialRegions is an array of region IDs for iteration for non-govcloud, non-CN region usage
+var CommercialRegions = []string{APNorthEast1, APNorthEast2, APSouth1, APSouthEast1, APSouthEast2, EUCentral1, EUWest1, SAEast1, USEast1, USWest1, USWest2}
+
+// GovRegions is an array of region IDs for iteration for GovCloud usage
+var GovRegions = []string{GovCloud}
+
+// CNRegions is an array of region IDs for iteration for CN Region usage
+var CNRegions = []string{CNNorth1}
